acee9870c9ebcaf1e138ac35f347098af9d07006
William Bugenis <wbugenis@gmail.com>
2021-01-19 11:16:48 -0500

    switching drivers

--- Walk_through/wt_animal.rb
+++ Walk_through/wt_animal.rb
@@ -1,3 +1,4 @@
+class Animal
 # identify relationship types
 # zoo ---< animal  zoo>-----<origin origin---<animals
 
@@ -5,13 +6,28 @@
 
 # set attr methods 
     # can't change species, name, origin
+    attr_reader :species, :name, :origin
     # zoo can be changed
+    attr_accessor :zoo
 
-# Declare Class variables
-
-# Define Initialize methods species, origin, name, and zoo 
+    # Declare Class variables
+    @@all = []
+    
+    # Define Initialize methods species, origin, name, and zoo 
     # - declare species, name, origin, zoo,  and push into all
+    def initialize(species, name, origin, zoo)
+        @species = species
+        @name = name
+        @origin = origin
+        self.zoo = zoo
+        self.class.all << self
+    end
 
-#  define self.all which reads @@all empty array
+    #  define self.all which reads @@all empty array
+    def self.all
+        @@all
+    end
+    
+    # This class defines and creates a new animal with a name, origin, species, and zoo.
 
-# This class defines and creates a new animal with a name, origin, species, and zoo.
+end
